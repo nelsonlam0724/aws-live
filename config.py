20025865cfc4e4d286ed451172335eddfbc4574e
nelsonlam0724 <151993432+nelsonlam0724@users.noreply.github.com>
2024-04-16 12:51:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cxawwuqou4a4.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "lamweihong-employee"
+customregion = "us-east-1"
+
